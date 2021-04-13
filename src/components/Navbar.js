c139db5671d658b1b37f145fd4cc9bc52819ef3f
Tash Alderson <aldersonnatasha@gmail.com>
2021-04-13 17:09:04 +0100

    navbar comp

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Navbar() {
 
-  return <section>
-    
+  return <section className="navbar">
+    <div className="nav-div">
+      <button className="nav-buttons" id="about-nav-button"><Link to={'/about-me/'}><p className="nav-text">ABOUT</p></Link></button>
+      <button className="nav-buttons" id="web-nav-button"><Link to={'/web-development/'}><p className="nav-text">DEVELOPMENT</p></Link></button>
+      <button className="nav-buttons" id="ad-nav-button"><Link to={'/advertising/'}><p className="nav-text">ADVERTISING</p></Link></button>
+    </div>
   </section>
 }
