e2a040c8abc865d948fe596f39329ca33d2e52c8
Tash Alderson <aldersonnatasha@gmail.com>
2021-04-14 18:30:47 +0100

    on click navbar change color

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ export default function Navbar() {
   const [advert, updateAdvert] = useState(false)
 
   function reload() { 
-    if (window.location.href === 'https://natasha-alderson.com/about-me/') {
+    if (window.location.href === 'http://localhost:8000/about-me/') {
       updateAbout(true)
       updateWeb(false)
       updateAdvert(false)
@@ -26,13 +26,12 @@ export default function Navbar() {
       updateWeb(false)
     }
   }
-  console.log(window.location.href)
 
   return <section className="navbar">
     <div className="nav-div">
-      <button onClick={reload} className={about ? 'nav-buttons dark' : 'nav-buttons'} id="about-nav-button"><Link to={'/about-me/'}><p className="nav-text">ABOUT</p></Link></button>
-      <button onClick={reload} className={web ? 'nav-buttons dark' : 'nav-buttons'} id="web-nav-button"><Link to={'/web-development/'}><p className="nav-text">DEVELOPMENT</p></Link></button>
-      <button onClick={reload} className={advert ? 'nav-buttons dark' : 'nav-buttons'} id="ad-nav-button"><Link to={'/advertising/'}><p className="nav-text">ADVERTISING</p></Link></button>
+      <button onClick={reload} className={about ? 'nav-buttons dark' : 'nav-buttons'} id="about-nav-button"><Link to={'/about-me/'}><p onClick={reload} className={about ? 'nav-text click' : 'nav-text'}>ABOUT</p></Link></button>
+      <button onClick={reload} className={web ? 'nav-buttons dark' : 'nav-buttons'} id="web-nav-button"><Link to={'/web-development/'}><p onClick={reload} className={about ? 'nav-text click' : 'nav-text'}>DEVELOPMENT</p></Link></button>
+      <button onClick={reload} className={advert ? 'nav-buttons dark' : 'nav-buttons'} id="ad-nav-button"><Link to={'/advertising/'}><p onClick={reload} className={about ? 'nav-text click' : 'nav-text'}>ADVERTISING</p></Link></button>
     </div>
   </section>
 }
