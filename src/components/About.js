9d1258d8ad4a6185f219d7da1045fcab5b9a02d0
Tash Alderson <aldersonnatasha@gmail.com>
2021-04-15 16:32:35 +0100

    tonne of media queries

--- src/components/About.js
+++ src/components/About.js
@@ -2,16 +2,19 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import profile from '../images/profile.jpg' 
 
+// {`/${profile}`}
+
+
 export default function About() {
 
 
   return <section>
     <div className="about-section">
       <div className="about-me">
-        <p className="about-me-info">I am a Junior Full-Stack Software Developer with experience as an Advertising Agency Producer where I worked in both London and New York running big Digital and TV campaigns.</p>
+        <p className="about-me-info">I am a Junior Full-Stack Software Developer with experience as an Advertising Producer where I worked in both London and NYC running big Digital and TV campaigns.</p>
         <button  className="back-button" id="about-back"><Link to={'/'}><h1 className="back-button" id="about-back-text">✕</h1></Link></button>
       </div>
-      <img className="profile-pic" src={`/${profile}`}/>
+      <img className="profile-pic" src="../images/profile.jpg"/>
       <p className="about-email">aldersonnatasha@gmail.com</p>
       <p className="skills">JavaScript | ReactJS | HTML5 | CSS | SCSS | Python | Flask | Express | Node.js | Mongoose | MongoDB | Git | Github | Jest | Mocha | <br></br>Chai | pytest | ProgresSQL | Babel | Chrome | Heroku | NPM | Safari | Visual Studio Code | SSH | SQLAlchemy | React-Testing-Library</p>
     </div>
